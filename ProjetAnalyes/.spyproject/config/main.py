f72f10a93e56e6f4e8630568af8f709bfb248b1e
YacineProg <yacineaoumara2@gmail.com>
2023-10-20 17:21:41 +0100

    Q5

--- ProjetAnalyes/.spyproject/config/main.py
+++ ProjetAnalyes/.spyproject/config/main.py
@@ -23,15 +23,22 @@ final_data = pd.read_csv(file_name)
 #here start 
 print("\n  ----------------- This is My Data ------------\n",final_data,"\n")
 
-# Calcul de la moyenne mensuelle des ventes 
-monthly_mean = final_data.groupby('Month')['Sales'].mean()
 
-# Visualisation de la moyenne mensuelle des ventes
-plt.figure(figsize=(10, 6))
-plt.bar(monthly_mean.index, monthly_mean.values, color='skyblue')
+# Calculate the moving average
+window_size = 5  # Adjust the window size as needed
+final_data['Moving_Avg'] = final_data['Sales'].rolling(window=window_size).mean()
+
+# Visualize the trend using the moving average
+plt.figure(figsize=(13, 6))
+for year in range(start_year, end_year+1):
+    year_data = final_data[final_data['Year'] == year]
+    plt.plot(year_data['Month'], year_data['Moving_Avg'], label=f'Moving Average for Year {year}')
+
 plt.xlabel('Month')
-plt.ylabel('Average Sales')
-plt.title('Average Sales per Month')
+plt.ylabel('Moving Average of Sales')
+plt.title('Trend of Sales using Moving Average')
+plt.legend()
 plt.xticks(rotation=30)
 plt.tight_layout()
 plt.show()
+print(final_data)
